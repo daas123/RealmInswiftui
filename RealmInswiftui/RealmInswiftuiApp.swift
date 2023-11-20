50d425bd5de7f9ab747a41ae0a3b76fe676defed
saad <saadneosoft@gmail.com>
2023-11-20 11:21:56 +0530

    Initial Commit

---
+++ RealmInswiftui/RealmInswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmInswiftuiApp.swift
+//  RealmInswiftui
+//
+//  Created by Neosoft on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct RealmInswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
